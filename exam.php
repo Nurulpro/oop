938a42ae7a14346ab62225ce3adda4a591654519
Nurul Islam <73928191+Nurulpro@users.noreply.github.com>
2021-06-12 20:00:38 +0600

    newexm

---
+++ exam.php
@@ -0,0 +1,97 @@
+<?php
+// $a=5;
+// $b=12;
+// $c=10;
+// $d=7;
+// $e=($a*$b)+$c*$d/$a;
+// print($e);
+
+
+
+// FunctionmodifyArray($array)
+
+// {
+// foreach ( $array as & $value)
+// {
+
+// $value = $value + 2;
+// }
+//     $value = $value + 3;
+
+// }
+
+// $array = array(1,2,3);
+// ModifyArray($array);
+// print_r($array);
+
+
+// $a = array (1,2,3);
+// $b = array(1>2,2>3,0>1);
+
+// $b = array( 'a'>1, 'b'> 2, 'c> 3');
+// var_dump($a==$b);
+// var_dump($a===$b);
+// var_dump($a==$c);
+
+
+// $x=25;
+// while($x<10)
+// {
+//     $x=30;
+// }
+// print($x);
+
+
+// echo (int)((0.1+0.7)*10);
+
+
+
+// 
+
+
+// switch (1) {
+//     case '1':print("Book Details");
+//     case '2':print("Book Autho");
+        
+    
+//     default:print("Missing Book");
+       
+       
+// }
+
+
+// $x=0;
+// $i;
+// for($i=0;$i<5;$i++)
+// {
+//     $x+=$i;
+// }
+
+// print($x);
+
+// answer:10
+
+
+// $a=20;
+// function myfunction($b){
+//     $a=30;
+//     global $a,$c;
+//     return $c=($b+$a);
+// }
+// print myfunction(40)+$c;
+
+// answer 120
+
+
+// $x = true and false and true;
+// var_dump($x);
+
+// answer true
+
+
+$i=(bool)"100";
+var_dump($i);
+
+?>
+
+
